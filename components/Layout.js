98c4233a194c93539f0590a29d1707efdd257f30
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-01-10 17:47:04 -0500

    change site title

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Aurelien Gremy';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Next.js Blog Website';
 
 export default function Layout({ children, home }) {
     return (
